6257e810d507327db28f9d4f604936fb0ae84c82
khoinguyenAdonis <nguyendinhkhoi1302@gmail.com>
2023-10-24 17:24:48 +0700

    24-10

--- exp4-ReplaceWord/main.c
+++ exp4-ReplaceWord/main.c
@@ -16,36 +16,93 @@ void upper(char *string){
         string++;
     }      
 }
-void findArray(char *arr, char *string){
-    uint8_t cozz = 0;
+int findArray(char *arr, char *string){
+    int count = 0;
     char *ptr = arr;
     while (*string != '\0'){
         if(*string == *arr ){
             while (*string == *arr || *arr == '\0'){
-                if(*string == ' ' && *arr != '\0' || *arr !='\0' && *string == '\0' || *arr == '\0' && *string != ' ' ){
-                    break;
+                if(*string == ' ' && *arr != '\0' || *arr !='\0' && *string == '\0' || *arr == '\0' && *string != ' ' ){                  
+                     break;
                 }
-                else if (*arr == '\0'){
-                    printf("Da tim thay chuoi da nhap !\nVi tri la: %d\n",cozz);
-                    return ;
-                }
+                else if (*arr == '\0'){                    
+                    return count;
+                }           
                 string++;
                 arr++;
-            }            
+            }count++ ;        
         }
-        cozz++;
+        count++;
         string++;
         arr = ptr;
 
     }
-    printf("Khong tim thay chuoi da nhap!\n");
+    return count = - 1;
 }
+uint8_t numberWord(char *arr){
+    uint8_t coutn = 0;
+    while (*arr != '\0')    {
+        coutn ++;
+        arr++;
+    }
+    return coutn;
+}
+void replaceWord(char *string, char *arr, char* arrReplace ){
+    int location = findArray(arr,string);
+    uint8_t numberReplace = numberWord(arr);
+    uint8_t numberString = numberWord(string);
+    uint8_t numberArrR = numberWord(arrReplace);
+    char* Sstring = string + location;
+    char* Aarayy = Sstring + numberReplace;
+    char array[numberString+numberArrR-numberReplace];
+    uint8_t index = 0;
+    if (location > -1){
+        while (string < Sstring){
+            array[index] = *string;
+            string++;
+            index++;
+        }
+        while(Sstring < Aarayy && *arrReplace != '\0'){
+            array[index] = *arrReplace; 
+            arrReplace++; 
+            index++;            
+            Sstring++; 
+            string++;        
+            if(*arrReplace == '\0' && Sstring< Aarayy){
+                while(Sstring< Aarayy){
+                    Sstring++;
+                    string++;
+                }       
+            }
+            else if (Sstring == Aarayy && *arrReplace != '\0'){
+                while(*arrReplace != '\0'){
+                    array[index] = *arrReplace;
+                    index ++;
+                    arrReplace++;          
+            }          
+                    
+        }}
+        while (*string != '\0'){
+            array[index] = *string;
+            string++;
+            index++;
+        }
+        if (*string == '\0')
+        {
+            array[index] = '\0';
+        }        
+        printf("%s\n", array);
 
+    }else printf("Khong tim thay chuoi can thay the\n");
+}
 int main (){
     char string[] = "to learn around the world learn to do annything. today we learn ab that";
- //   upper(string);
-    char f[] = "lear";
-    findArray(f,string);
+//    upper(string);
+    char f[] = "learn";
     printf("%s\n",string);
+    int location = findArray(f,string);
+    printf("Text: %s\n",f);
+    printf((location>-1)?"Ket qua: co\nVi Tri: %d\n":"ket qua: khong co\n", location);   
+    replaceWord(string,"ab","about");
     return 0;
 }
